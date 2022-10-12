2afa482f04181a30c86b4fda73555654ce056b58
Vimal Sharma <99383911+vimalsharma07@users.noreply.github.com>
2022-10-12 02:36:29 -0700

    Time: 1 ms (91.16%), Space: 40.3 MB (92.87%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,61 @@
+class Solution {
+    
+     public boolean buddyStrings(String str, String goal) {
+        int n= str.length();
+        int m= goal.length();
+        if(m!=n)return false;
+        int j=-1; int k=-1;
+        int an=0;
+        for(int i=0; i<n; i++){
+            char ch= str.charAt(i);
+            char sh= goal.charAt(i);
+            if(ch!=sh){
+                an++;
+                if(j==-1){
+                    j=i;
+                }else{
+                    k=i;
+                }
+            }
+        }
+        if(an>2 || an==1)return false;
+        
+        StringBuilder ans= new StringBuilder();
+        if(an==2){
+        for(int i=0; i<n; i++){
+            char ch= str.charAt(i);
+            if(i==j){
+                char temp= str.charAt(k);
+                ans.append(temp);
+            }else if(i==k){
+                char a= str.charAt(j);
+                ans.append(a);
+            }else{
+                ans.append(ch);
+            }
+        }
+        }else{
+            int[]fre= new int[26];
+            for(int i=0; i<n; i++){
+                char ch= str.charAt(i);
+                fre[ch-'a']++;
+            }
+            for(int z:fre){
+                if(z>=2)return true;
+            }
+            
+            
+        }
+        
+        
+      String fn= ans.toString()  ;
+        
+      return fn.equals(goal)  ;
+    }
+    
+    
+    public boolean areAlmostEqual(String s1, String s2) {
+        if(s1.equals(s2))return true;
+        return  buddyStrings(s1,s2);
+    }
+}
